a5f044050aa959d5029265f05ae353ad7ec330a3
YamYam <rwg.amos.dev@gmail.com>
2020-06-05 20:57:53 +0800

    fixed height testing

--- src/QuestionPage.js
+++ src/QuestionPage.js
@@ -15,12 +15,12 @@ import {
 export const QuestionPage = props => {
     let { node, setNode } = props;
     return (
-        <div class="main">
-            <div class="text-center">
-                <h1 class="customFont">Do I need insurance?</h1>
-                <p class="negativeMargin">a guide by the people of r/phinvest</p>
-            </div>
-                <div class="centered">
+        <div class="main centered">
+                <div class="title text-center">
+                    <h1 class="customFont">Do I need insurance?</h1>
+                    <p class="negativeMargin">a guide by the people of r/phinvest</p>
+                </div>
+               <div class="content">
                     <div class="yellowblack-text"> 
                         {node.message}
                     </div>
@@ -28,7 +28,7 @@ export const QuestionPage = props => {
                         {node.notes}
                     </div>
                 </div>
-                <div class="yes-no-buttons-div">
+                <div class="footer yes-no-buttons-div">
                     <div class="bottom">
                         <button class="option left yes" onClick = {() => setNode(node.yes)}>
                             <CheckOutlined /> 
